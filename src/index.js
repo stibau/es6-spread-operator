c2b19baddb47bbfe24e33960831fe131acf89caf
stibau <48186853+stibau@users.noreply.github.com>
2021-02-07 16:11:07 +0100

    examples of Spread Operator Usage

--- src/index.js
+++ src/index.js
@@ -3,3 +3,27 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const citrus = ["Lime", "Lemon", "Orange"];
+const fruits = ["Apple", "Banana", "Coconut", ...citrus];
+const fruits2 = ["Apple", ...citrus, "Banana", "Coconut"];
+console.log(fruits);
+console.log(fruits2);
+
+const fullName = { fName: "James", lName: "Bond" };
+
+const user = {
+  ...fullName,
+  id: 1,
+  username:"jamesbond007"
+}
+
+console.log(user);
+
+const user2 = {
+  fullName,
+  id: 1,
+  username:"jamesbond007"
+}
+
+console.log(user2);
